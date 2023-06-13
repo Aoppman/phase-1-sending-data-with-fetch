377728f7da2204a30a010332449817f4571b2444
Andrew <a.oppman@gmail.com>
2023-06-13 17:05:20 -0400

    code added to index.js

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your code here
+
+/*const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(contactInfo),
+};*/
+
+function submitData(name, email) {
+  const contactInfo = {
+    name: name,
+    email: email,
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(contactInfo),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (contactInfo) {
+      document.body.textContent = contactInfo.id;
+    })
+    .catch(function (error) {
+      document.body.textContent = error.message;
+    });
+
+  console.log(contactInfo);
+
+}
+
+submitData("Sam", "Sammy1@aol.org")
